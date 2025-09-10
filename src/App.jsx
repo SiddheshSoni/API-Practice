37f8b9af4e80c2c6722b79ee07a0745018279bce
SiddheshSoni <work.siddheshsoni@gmail.com>
2025-09-10 20:29:58 +0530

    Error Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,68 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import './App.css'
 
 
 function App() {
-  const API = "https://swapi.info/api/films"
+  const API = "https://swapi.info/api/films//"
   const [films, setFilms] = useState([])
-  const [isLoading, SetIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null)
+  const [isRetrying, setIsRetrying]= useState(false);
 
+  const intervalRef = useRef(null);
 
   async function fetchData(){
-    SetIsLoading(true);
-    console.log(isLoading)
-    
+    setIsLoading(true);
+    setError(null);
+
     try{
       const res = await fetch(API);
+      
+      if(!res.ok){
+        throw new Error("Something Went Wrong!!")
+      }
+
+      if(intervalRef){
+        clearInterval(intervalRef);
+        setIsRetrying(false);
+      }
       const data = await res.json();
-      // console.log(data);
-      setFilms(data);
-      SetIsLoading(false);
-      console.log(isLoading)
+
+      const filmsdata = data.map((film) => (
+        <div key={film.url}>{film.title}</div>
+        ));
+
+      setFilms(filmsdata);
+
+      //added setTimout just for the cosmetics
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1500);
     }
     catch(err){
       console.log(err);
+      setError("Something Went Wrong! ...Retrying");
+      setIsLoading(false);
+
+      if(!intervalRef.current){
+        setIsRetrying(true);
+        intervalRef.current = setInterval(() => {
+          fetchData();
+        }, 5000);
+      }
     }
   }
+
+  function cancelRetry() {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+    setIsRetrying(false);
+    setError("Retrying cancelled by user.");
+  }
+
   return (
     <>
       <button onClick={fetchData}>Fetch</button>
@@ -32,10 +70,15 @@ function App() {
           {isLoading && <span>Loading...</span>}
       </div>
       <div>
-        {console.log(isLoading)}
-        {films.map((film) => (
-          <div key={film.url}>{film.title}</div>
-        ))}
+        {isRetrying && (
+        <button onClick={cancelRetry} style={{ marginTop: "10px" }}>
+          Cancel
+        </button>
+      )}
+      </div>
+      <div>
+        {!isLoading && films}
+        {error}
       </div>
     </>
   )
