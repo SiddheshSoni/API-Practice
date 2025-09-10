07b341d733cbfd47a51148468f7059c7dfcf33fe
SiddheshSoni <work.siddheshsoni@gmail.com>
2025-09-10 19:40:13 +0530

    added a loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,20 @@ import './App.css'
 function App() {
   const API = "https://swapi.info/api/films"
   const [films, setFilms] = useState([])
-  
+  const [isLoading, SetIsLoading] = useState(false);
+
+
   async function fetchData(){
+    SetIsLoading(true);
+    console.log(isLoading)
+    
     try{
       const res = await fetch(API);
       const data = await res.json();
-      console.log(data);
+      // console.log(data);
       setFilms(data);
+      SetIsLoading(false);
+      console.log(isLoading)
     }
     catch(err){
       console.log(err);
@@ -22,6 +29,10 @@ function App() {
     <>
       <button onClick={fetchData}>Fetch</button>
       <div>
+          {isLoading && <span>Loading...</span>}
+      </div>
+      <div>
+        {console.log(isLoading)}
         {films.map((film) => (
           <div key={film.url}>{film.title}</div>
         ))}
